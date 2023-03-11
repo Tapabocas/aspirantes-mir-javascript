26e1557089c11453ec6ad7b43f131c569b0096fe
MSI_David <david-cpr@hotmail.com>
2023-03-10 19:43:19 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+function hola(nombre){
+    Saludo=("Hola "+nombre+"!")
+    return Saludo
+}
+
+console.log(hola("Pedro")) 
+console.log(hola("Juan")) 
+console.log(hola("")) 
---
+++ js-1/sum.js
@@ -0,0 +1,8 @@
+function sum (number){
+    var gauss=(number)*(number+1)/2
+    return gauss
+}
+
+ console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+ console.log(sum(10)) // 55
+ console.log(sum(15)) // 120
